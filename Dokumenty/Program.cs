01ec6e184ec529e5477cd7fe1ed3204871f9fb88
kazdej123 <kamilmurawski92@gmail.com>
2019-06-28 16:50:27 +0200

    Pierwszy commit

---
+++ Dokumenty/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Documents
+{
+	sealed class Program
+	{
+		private static void Main(string[] args)
+		{
+			object documents = new System.Object();
+			PrintDocuments(documents);
+		}
+
+		private static void PrintDocuments(object documents)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
